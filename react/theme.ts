02a6a5edec563d9a7fecf75774e1a1d6e0ea0aa8
nozomi <nozomi.ishii@bitkey.jp>
2020-11-23 02:06:28 +0900

    add global style

--- react/theme.ts
+++ react/theme.ts
@@ -105,6 +105,8 @@ export const GlobalStyle = createGlobalStyle`
 		margin: 0;
     padding: 0;
     box-sizing: border-box;
+    text-decoration: none;
+    list-style:none;
   }
 	body{
 		min-height: 100vh;
